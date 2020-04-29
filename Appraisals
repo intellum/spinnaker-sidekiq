7f2c5806c20a8271242d192ab77fc39a77109084
Jorge Dias <jdias@intellum.com>
2020-04-29 11:55:51 +0200

    Run tests against correct rails versions

--- Appraisals
+++ Appraisals
@@ -1,15 +1,16 @@
 appraise "rails-5.0" do
-  gem "rails", "~> 5.0"
+  gem "rails", "~> 5.0.0"
+  gem "sqlite3", "~> 1.3.6"
 end
 
 appraise "rails-5.1" do
-  gem "rails", "~> 5.1"
+  gem "rails", "~> 5.1.0"
 end
 
 appraise "rails-5.2" do
-  gem "rails", "~> 5.2"
+  gem "rails", "~> 5.2.0"
 end
 
 appraise "rails-6.0" do
-  gem "rails", "~> 6.0"
+  gem "rails", "~> 6.0.0"
 end
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -8,11 +8,11 @@ require "spinnaker/sidekiq"
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
+    if respond_to?(:load_defaults)
+      config.load_defaults Rails::VERSION::MAJOR
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
-
